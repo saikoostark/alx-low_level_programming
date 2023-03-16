c1ef4fbeb7acbf1c5d303e0a9f0d4a50ee562ef5
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-16 07:31:42 +0200

    added files

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-  fprint(stderror , "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n")
+  fprintf(stderror , "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n")
   return (0);
 }
 
