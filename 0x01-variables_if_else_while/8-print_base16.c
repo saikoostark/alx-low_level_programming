b789c11d718d135849fc14f41d1247f40272c8fe
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-17 09:48:38 +0200

    added files

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0 ; i < 16 ; i++)
+	{	if (i < 10)
+			putchar('0' + i);
+		else
+			putchar('a' + i % 10);
+	}
+	putchar('\n');
+	return (0);
+}
+
