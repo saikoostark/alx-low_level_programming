7418589df3daa69214d4372b8cf1e1c37dccdb48
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-17 09:54:33 +0200

    added files

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0 ; i < 10 ; i++)
+	{
+		putchar('0' + i);
+		if (i == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
