7247cb3ea5d1c444dbc524c8d9640b739e1c75e1
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-17 09:30:41 +0200

    added files

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0 ; i < 26 ; i++)
+		putchar(97 + i);
+	for (int i = 0 ; i < 26 ; i++) 
+		putchar(65 + i)
+	putchar('\n');
+	return (0);
+}
+
