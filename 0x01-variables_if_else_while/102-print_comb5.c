45ac5334514f3b7a0eacd8a9de1e94cf2efb5092
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-17 12:07:42 +0200

    added files

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,14 +13,13 @@ int main(void)
 	for (i = 0; i < 100; i++)
 	{
 		for (j = i+1; j < 100; j++) {
-
-			putchar('0' + (i/10 % 10));
+			putchar('0' + (i / 10 % 10));
 			putchar('0' + (i % 10));
 			putchar(' ');
-			putchar('0' + (j/10 % 10));
+			putchar('0' + (j / 10 % 10));
 			putchar('0' + (j % 10));
 			
-			if(i == 98 && j == 99)
+			if (i == 98 && j == 99)
 				continue;
 
 			putchar(' ');
@@ -33,39 +32,3 @@ int main(void)
 
 	return (0);
 }
-
-// int main(void)
-// {
-// 	int i, j, k, l;
-//
-// 	for (i = 0 ; i < 10 ; i++)
-// 	{
-// 		for (j = 0 ; j < 10 ; j++)
-// 		{
-// 			for (k = i ; k < 10 ; k++)
-// 			{
-// 				for (l = j + 1 ; l < 10 ; l++)
-// 				{
-// 					putchar('0' + i);
-// 					putchar('0' + j);
-// 					putchar(' ');
-// 					putchar('0' + k);
-// 					putchar('0' + l);
-//
-// 					if (i == 9 && j == 8 && k == 9 && l == 9)
-// 						continue;
-//
-// 					putchar(' ');
-// 					putchar(',');
-// 				}
-// 			}
-// 		}
-// 	}
-// 	putchar('\n');
-// 	return (0);
-// }
-//
-
-
-
-
