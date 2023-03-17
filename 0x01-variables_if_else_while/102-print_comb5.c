8e565478ddd0641a4a3caf5f3e79a41f616ab6e2
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-17 12:11:18 +0200

    added files

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,19 +12,20 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = i+1; j < 100; j++) {
+		for (j = i + 1; j < 100; j++)
+		{
 			putchar('0' + (i / 10 % 10));
 			putchar('0' + (i % 10));
 			putchar(' ');
 			putchar('0' + (j / 10 % 10));
 			putchar('0' + (j % 10));
-			
+
 			if (i == 98 && j == 99)
 				continue;
 
 			putchar(' ');
 			putchar(',');
-				
+
 		}
 	}
 	
