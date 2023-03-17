b6ea0f38904825cb5efb87ca1026e74648dce2f5
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-17 11:33:24 +0200

    added files

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-	for (int i = 0 ; i < 10 ; i++)
+	int i, j, k, l;
+	for (i = 0 ; i < 10 ; i++)
 	{
-		for (int j = 0 ; j < 10 ; j++)
+		for (j = 0 ; j < 10 ; j++)
 		{
-			for (int k = i ; k < 10 ; k++)
+			for (k = i ; k < 10 ; k++)
 			{
-				for (int l = j + 1 ; l < 10 ; l++)
+				for (l = j + 1 ; l < 10 ; l++)
 				{
 					putchar('0' + i);
 					putchar('0' + j);
