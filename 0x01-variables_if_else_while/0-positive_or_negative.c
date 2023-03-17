4ac12d6863659c9d385a3a4130e515a5cb46816a
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-17 11:21:55 +0200

    added files

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,10 @@
 
 /* betty style doc for function main goes there */
 
-/**                                                                                   │
-	* main - Entry point                                                                 │
-	*                                                                                    │
-	* Return: Always 0 (Success)                                                         │
+/**
+	* main - Entry point 
+	*
+	* Return: Always 0 (Success)
 */
 int main(void)
 {
