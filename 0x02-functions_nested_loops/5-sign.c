7a3336005e47e92bf6301375d69ecd0d811712c8
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-21 22:19:13 +0200

    updating

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - function here
+ * @n: parameter of function
+ * Return: 1 if it is lower
+ * and return 0 else
+ */
+
+
+int print_sign(int n)
+{
+	if (n >0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n<0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
