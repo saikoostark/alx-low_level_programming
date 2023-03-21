2baacfb73d7be8e27b56d94c018054fbb52afa6e
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-21 22:34:59 +0200

    updating

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - function here
+ * @n: parameter of function
+ * Return: 1 if it is lower
+ * and return 0 else
+ */
+
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
