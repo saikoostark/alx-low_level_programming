8f9fd69d2b197ea3242b605422388a9db8a3ee1e
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-21 22:07:58 +0200

    updating

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - function here
+ * @c: parameter of function
+ * Return: 1 if it is lower
+ * and return 0 else
+ */
+
+
+int _isalpha(int c)
+{
+	if ( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
