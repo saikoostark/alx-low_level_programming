26606b6da305f48caf672c7784ca193b554e2a89
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-21 22:27:51 +0200

    updating

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - function here
+ * @n: parameter of function
+ * Return: 1 if it is lower
+ * and return 0 else
+ */
+
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
