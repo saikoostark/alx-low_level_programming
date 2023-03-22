259568ace43cd9703ce289cddaa3a1fda5d40c66
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-22 13:44:14 +0200

    updating

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-long int i, n1, sum, tem;
+unsigned long i, n1, sum, tem;
 
 n1 = 1, sum = 0;
 
 for (i = 1 ; i < 99 ; i++)
 {
-printf("%ld", sum + n1);
+printf("%lu", sum + n1);
 tem = n1;
 n1 = sum + n1;
 sum = tem;
