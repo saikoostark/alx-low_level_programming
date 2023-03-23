8a0424c414913b7de1aa69953bafc93a0451e55e
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-24 00:30:43 +0200

    updating

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,15 +10,26 @@
 void print_number(int n)
 {
 
-int reversedNum;
+int reversedNum, zer, x;
 
-reversedNum = 0;
+
+reversedNum = 0, zer = 0 , x = 1;
 
 while (n != 0)
 {
+
+if (x && !(n % 10))
+{
+zer++;
+n /= 10;
+continue;
+}
+
+x = 0;
 reversedNum = reversedNum * 10 + n % 10;
 n /= 10;
 }
+
 
 /**/
 if (reversedNum < 0)
@@ -35,5 +46,12 @@ while (reversedNum)
 _putchar('0' + reversedNum % 10);
 reversedNum /= 10;
 }
+
+
+while (zer--)
+{
+	_putchar('0');
+}
+
 
 }
