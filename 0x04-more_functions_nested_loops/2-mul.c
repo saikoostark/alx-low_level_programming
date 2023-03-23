6c4e4400a10dbed5cb33ec1a49d7263f96b95d80
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-23 13:38:57 +0200

    updating

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - function here
+ * @a: parameter 1 of function
+ * b: parameter 2 of function
+ * Return: 1 if it is lower
+ * and return 0 else
+ */
+
+
+int mul(int a, int b)
+{
+return (a * b);
+}
