c966e19396d0296d22afaa13d1292bf603744066
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-23 13:46:45 +0200

    updating

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function here
+ * Return: 1 if it is lower
+ * and return 0 else
+ */
+
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 0 ; i < 10 ; i++)
+_putchar('0' + i);
+_putchar('\n');
+}
