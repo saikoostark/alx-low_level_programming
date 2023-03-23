60a6fd12c00e862a210a2204c33eb6269295d952
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-23 14:00:29 +0200

    updating

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - function here
+ * @n: param
+ * Return: 1 if it is lower
+ * and return 0 else
+ */
+
+
+void print_line(int n)
+{
+int i;
+
+for (i = 0 ; i < n ; i++)
+_putchar('-');
+_putchar('\n');
+}
