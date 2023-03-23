75d995ed2d0002fa25200ebbf7d32940f54ed336
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-23 13:29:32 +0200

    updating

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - function here
+ * @c: parameter of function
+ * Return: 1 if it is lower
+ * and return 0 else
+ */
+
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
