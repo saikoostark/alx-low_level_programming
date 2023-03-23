ef0048723c04a4f1ef0098060b0503b0740defc1
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-23 11:12:11 +0200

    updating

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+	* positive_or_negative - Entry point
+	* @n: param 1
+	* Return: Always 0 (Success)
+*/
+void positive_or_negative(int n)
+{
+
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+}
+
