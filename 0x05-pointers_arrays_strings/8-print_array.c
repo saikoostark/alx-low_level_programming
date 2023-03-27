4588dac932d524b5834313f8792b68930a9599a1
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-27 17:23:32 +0200

    updating

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - function
+ * @str: param1
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_array(int *a, int n)
+{
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+			printf(", ");
+	}
+
+	printf("\n");
+}
