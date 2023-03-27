751e138f8fea9b14218e9b1b045af85958de88cc
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-27 17:49:02 +0200

    updating

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - function
+ * @dest: array
+ * @src: size
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (src);
+}
