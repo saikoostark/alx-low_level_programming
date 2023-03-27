80759ad70b918a395500d13473e2785bddbb9822
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-27 15:32:14 +0200

    updating

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * reset_to_98 - function
+ * @n: paramt
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
