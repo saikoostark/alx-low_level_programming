f492b4878a9b75bbb5c15046c8deeac58f2aa1b1
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-27 17:06:00 +0200

    updating

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * rev_string - function
+ * @s: param1
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void puts2(char *str)
+{
+
+	
+}
