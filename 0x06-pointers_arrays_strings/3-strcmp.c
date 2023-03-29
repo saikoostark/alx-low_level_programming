157f9e1d112190c28b37734d3df6b9554baa8051
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-29 22:57:19 +0200

    updating

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcmp - function
+ * @s1 : param1
+ * @s2 : param 2
+ *Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, sum1, sum2;
+
+	i = sum1 = sum2 = 0;
+
+	while (*(s1 + i))
+		sum1 += *(s1 + i++);
+
+	i = 0;
+
+	while (*(s2 + i))
+		sum2 += *(s2 + i++);
+
+	if (sum1 - sum2 > 0)
+		return (15);
+	else if (sum1 - sum2 < 0)
+		return (-15);
+	else
+		return (0);
+}
