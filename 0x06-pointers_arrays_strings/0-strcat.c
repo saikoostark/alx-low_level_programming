8817071f27a6694aa4d183db94f3363f4296ff57
saikoo-stark <alwkylsyd87@gmail.com>
2023-03-29 16:32:40 +0200

    updating

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - function
+ * @dest : param1
+ * @src : param 2
+ * Return : pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int size, i;
+
+	size = 0, i = 0;
+
+	while (*(dest + size))
+		size++;
+
+	while (*(src + i))
+		dest[size++] = src[i++];
+
+	dest[size] = '\0';
+
+	return dest;
+}
