bb9cda4ee76c85593416ad3595e75098634fb430
saikoo-stark <alwkylsyd87@gmail.com>
2023-04-06 21:45:13 +0200

    updating

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc : 1st param
+ * @argv : 2nd param
+ * Return: Always 0.
+ */
+
+int main(int argc, char const *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
