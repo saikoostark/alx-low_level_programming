4a36e900cf2746601515469e924b809d3e00a5cb
saikoo-stark <alwkylsyd87@gmail.com>
2023-04-06 18:07:26 +0200

    updating

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _strlen_recursion - writes the character c to stdout
+ * @s: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (!*s)
+		return 0;
+
+	return 1 + _strlen_recursion(s + 1);
+}
