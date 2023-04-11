7748ab1c8055c0fe89ed04e2250c58ab24536a43
saikoo-stark <alwkylsyd87@gmail.com>
2023-04-11 21:05:30 +0200

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - check the code
+ * @str : 1st param
+ * Return: Always 0.
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+		i++;
+
+	return (i);
+}
+
+/**
+ * _strdup - check the code
+ * @str : 1st param
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+	int size;
+	int *ptr;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = _strlen(str);
+
+	ptr = malloc(size * sizeof(char) + 1);
+
+	if (ptr == NULL)
+		return NULL;
+
+	while (*(str + i))
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+
+	ptr[i] = '\0';
+	return (ptr);
+}
