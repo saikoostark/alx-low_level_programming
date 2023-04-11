8217cfc6473511c7fa7a0d8e917f9a68f64d7833
saikoo-stark <alwkylsyd87@gmail.com>
2023-04-11 20:59:02 +0200

    a

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *ptr = (char *)malloc(sizeof(char) * size);
 
-	if (ptr == NULL)
+	if (ptr == NULL || size == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
