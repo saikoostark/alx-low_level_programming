cc5a49e40e3512ce0a431e056ed2080a63b1e115
saikoo-stark <alwkylsyd87@gmail.com>
2023-04-11 22:42:44 +0200

    q

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - check the code
+ * @str : 1st param
+ * Return: Always 0.
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	if (str == NULL)
+		return (0);
+
+	while (*(str + i))
+		i++;
+
+	return (i);
+}
+
+/**
+ * argstostr - check the code
+ * @ac : param
+ * @av : param
+ * Return: Always 0.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i = 0, j = 0, k = 0, sum = 0;
+	char *str;
+
+	for (i = 0; i < ac; i++)
+		sum += _strlen(av[i]);
+
+	str = malloc(sizeof(char) * sum + (ac));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		while (*(av[i] + j))
+		{
+			str[k++] = av[i][j];
+		}
+		str[k++] = '\0';
+	}
+
+	return (str);
+}
