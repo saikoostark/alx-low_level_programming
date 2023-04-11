331f9fcc7d68946550675f4eef898a7a4854649b
saikoo-stark <alwkylsyd87@gmail.com>
2023-04-11 21:11:57 +0200

    a

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - check the code
+ * @str : 1st param
+ * Return: Always 0.
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+		i++;
+
+	return (i);
+}
+
+/**
+ * str_concat - check the code
+ * @s1 : param
+ * @s2 : param
+ * Return: Always 0.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+	int size, i = 0, j = 0, k = 0;
+	char *ptr;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	ptr = malloc(size * sizeof(char) + 1);
+
+	if (ptr == NULL)
+		return NULL;
+
+	while (*(s1 + j))
+		ptr[i++] = s1[j++];
+
+	while (*(s2 + k))
+		ptr[i++] = s1[k++];
+
+	ptr[i] = '\0';
+
+	return (ptr);
+}
