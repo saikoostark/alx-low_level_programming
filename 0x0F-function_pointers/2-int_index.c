4aec355900a89c5be1afe66c8065bdcf6ea85c49
saikoo-stark <alwkylsyd87@gmail.com>
2023-04-19 21:13:04 +0200

    a

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - func
+ *
+ * @array: param
+ * @size: param
+ * @action: cmp
+ * Return: retun nothing at all
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t i;
+
+	if (array && cmp && size > -1)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return i;
+		}
+	}
+}
