451d31c7d17b8ba718692da0c4568bc80bf3f0d2
saikoo-stark <alwkylsyd87@gmail.com>
2023-04-19 21:04:12 +0200

    a

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - func
+ *
+ * @array: param
+ * @size: param
+ * @action: param
+ * Return: retun nothing at all
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
