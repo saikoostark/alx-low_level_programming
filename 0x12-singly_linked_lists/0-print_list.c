08ea68346d1aa9f4d9524af09caad43e945b9ce9
saikoo-stark <alwkylsyd87@gmail.com>
2023-04-27 15:06:40 +0200

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,17 @@
 size_t print_list(const list_t *h)
 {
 
-	int listSize = 0;
+	size_t listSize = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
-		listSize++;
+
 		h = h->next;
+		listSize++;
 	}
 
 	return (listSize);
