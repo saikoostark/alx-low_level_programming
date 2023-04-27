6c89224005ae368d2927f72b2d8b717e54fa0d90
saikoo-stark <alwkylsyd87@gmail.com>
2023-04-27 14:18:27 +0200

    a

---
+++ 0x12-singly_linked_lists/assemply.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * @argc: param
+ * @argv: param
+ * Return: 0 on success
+ */
+int main(int argc, char const *argv[])
+{
+	printf("Hello, Holberton\n");
+	return (0);
+}
