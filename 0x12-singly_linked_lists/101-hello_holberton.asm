e2574813b9abb301c5929bf8c56e728f1cb603a8
saikoo-stark <alwkylsyd87@gmail.com>
2023-04-27 14:25:11 +0200

    a

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,19 @@
-section .data
-    hello: db 'Hello, Holberton', 0
-    format: db '%s', 10, 0
+    section .data
+hello:	db "Hello, Holberton",10,0
+format:	db "%s",0
 
-section .text
+    section .text
     global main
+    extern printf
 
 main:
     push rbp
     mov rbp, rsp
 
-    mov rdi, format
     mov rsi, hello
-    xor eax, eax
+    mov rdi, format
     call printf
 
-    mov rsp, rbp
     pop rbp
-    xor eax, eax
+    mov rax, 0
     ret
