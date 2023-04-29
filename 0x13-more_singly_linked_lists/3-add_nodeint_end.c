c34dda31c4f87e59119bec8c355337edd2628818
saikoo-stark <alwkylsyd87@gmail.com>
2023-04-29 12:19:42 +0200

    a

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,6 +16,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 
 	temp = malloc(sizeof(listint_t));
+
+	if (temp == NULL)
+		return (NULL);
+
 	ptr = *head;
 	temp->n = (int)n;
 
