5ba3b751268fda5926d66f863bfd1a94cfde1a13
SaikooStark <alwkylsyd87@gmail.com>
2023-05-04 21:02:21 +0300

    a

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function
+ * @b: param
+ * Return: return 0 on success
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int x = 0;
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == 0)
+			x = x >> 1;
+		else if (b[i] == 1)
+		{
+			x = x >> 1;
+			x = x | 1;
+		}
+		else
+			return (0);
+	}
+
+	return (x);
+}
