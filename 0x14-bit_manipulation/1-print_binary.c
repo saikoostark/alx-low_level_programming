0022f59ff6636584d33307dc651f5c039af20cb5
SaikooStark <alwkylsyd87@gmail.com>
2023-05-04 21:24:17 +0300

    a

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * helper - function
+ * @n: param
+ * Return: return 0 on success
+ */
+void helper(unsigned long int n)
+{
+	if (n != 0)
+	{
+		helper(n >> 1);
+		_putchar('0' + (n & 1));
+	}
+}
+
+/**
  * print_binary - function
  * @n: param
  * Return: return 0 on success
@@ -8,9 +22,9 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n != 0)
-	{
-		print_binary(n >> 1);
-		_putchar('0' + (n & 1));
-	}
+
+	if (n == 0)
+		_putchar('0');
+	else
+		helper(n);
 }
