199a929e14aef32a02ce8cbdb131b456c65d1624
Saikoo_Stark <alwkylsyd87@example.com>
2023-08-17 14:28:05 +0300

    saikoo commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,19 +6,18 @@
  * @n: param
  * Return: returning something
  */
-
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 
-    dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
-    if (head == NULL || new == NULL)
-        return (NULL);
+	if (head == NULL || new == NULL)
+		return (NULL);
 
-    new->n = (int)n;
-    new->prev = NULL;
-    new->next = *head;
+	new->n = (int)n;
+	new->prev = NULL;
+	new->next = *head;
 
-    *head = new;
-    return (new);
+	*head = new;
+	return (new);
 }
